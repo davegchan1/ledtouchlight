612dab9adb80ec953fceadfa20a451341ad04be7
davegchan1 <davegchan@gmail.com>
2017-04-21 14:37:05 -0700

    Add files via upload

---
+++ Working_capsense_3_sensors_3_leds_turns_on_and_off.ino
@@ -0,0 +1,121 @@
+
+#include <CapacitiveSensor.h>
+
+
+long time = 0;
+int state1 = LOW; // flipping to low tp see if it will disable led turn on boot seq
+boolean yes1;
+boolean previous1 = false;
+int debounce1 = 200;
+
+#define REDPIN 3
+#define GREENPIN 11
+#define BLUEPIN 9
+
+int state2 = LOW;
+boolean yes2;
+boolean previous2 = false;
+int debounce2 = 200;
+
+int state3 = LOW;
+boolean yes3;
+boolean previous3 = false;
+int debounce3 = 200;
+
+CapacitiveSensor   cs_2_12 = CapacitiveSensor(2, 12);  // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired// To add more sensors...//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foilvoidsetup()
+CapacitiveSensor   cs_5_6 = CapacitiveSensor(5, 6);       //red  2M resistor between pins 5 & 6, pin 5 is sensor pin, add a wire and or foil if desired
+CapacitiveSensor   cs_7_8 = CapacitiveSensor(7, 8);       // green 3.3M resistor between pins 7 & 8, pin 7 is sensor pin, add a wire and or foil if desired
+
+void setup()
+{
+  cs_2_12.set_CS_AutocaL_Millis(0xFFFFFFFF);  //Calibrate the sensor...
+  cs_5_6.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 2 - just as an example
+  cs_7_8.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 3 - just as an example
+
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  Serial.begin(9600); 
+}
+
+void loop()
+{
+
+  long start = millis(); // dc added
+  long total1 =  cs_2_12.capacitiveSensor(30);
+  long sensorValue1 =  cs_2_12.capacitiveSensor(30); 
+  long total2 =  cs_5_6.capacitiveSensor(30);
+  long sensorValue2 =  cs_5_6.capacitiveSensor(30);
+  long total3 =  cs_7_8.capacitiveSensor(30);
+  long sensorValue3 =  cs_7_8.capacitiveSensor(30);
+
+  Serial.print(sensorValue1);  // tab character for debug windown spacing  
+  Serial.print(sensorValue2);                    // tab character for debug windown spacing
+  Serial.print(sensorValue3);                    // tab character for debug windown spacing
+
+  Serial.print(millis() - start);        // check on performance in milliseconds dc added
+
+
+  Serial.print(total1);                  // print sensor output 1
+  Serial.print("\t");
+  Serial.print(total2);                  // print sensor output 2
+  Serial.print("\t");
+  Serial.print(total3);                  // print sensor output 3
+  Serial.println("\t");
+
+  if (total1 > 600) {yes1 = true;}
+  else {yes1 = false; // to toggle the state of state
+  }
+  if (yes1 == true && previous1  == false && (millis() - time > debounce1 || millis() < time)) {
+
+  if (state1 == HIGH) {state1 = LOW;}
+  else
+  state1 = HIGH;
+    time = millis();
+  }
+  digitalWrite(GREENPIN, state1);
+  previous1 = yes1;
+  
+
+  if (total2 > 600) {yes2 = true;}
+  else {yes2 = false; // to toggle the state of state
+  }
+  if (yes2 == true && previous2  == false && (millis() - time > debounce2 || millis() < time)) {
+
+    if (state2 == HIGH) {state2 = LOW;}
+    else
+      state2 = HIGH;
+    time = millis();
+  }
+
+  digitalWrite(REDPIN, state2);
+  previous2 = yes2;
+  
+  {
+    if (total3 > 600) {yes3 = true;
+    }
+    else {yes3 = false; // to toggle the state of state
+    }
+    if (yes3 == true && previous3  == false && (millis() - time > debounce3 || millis() < time)) {
+
+      if (state3 == HIGH) {state3 = LOW;}
+      else
+        state3 = HIGH;
+      time = millis();
+    }
+
+    digitalWrite(BLUEPIN, state3);
+    previous3 = yes3;
+    {
+      Serial.println(millis() - time);
+      delay(10);
+
+
+    }
+  }
+}
+
+
+
+
+
---
+++ working_capsense_3_sensor_3_led_on_off_fading.ino
@@ -0,0 +1,179 @@
+// video and site are not in english. code was copied from a translated site. 
+
+// you tube https://www.youtube.com/watch?v=IGZt8YidUsE
+// code pulled from : http://crepusculotecnologico.blogspot.com.br/2012/04/interruptor-touch-diagrama-e-codigo.html
+
+// working sensor, working on and off fade.  ( single color )
+// all 3 sensors fade on and off. moved closing curly bracket to close first cap 10 statement,
+/* 
+ *  youtube pushbutton and fading led with arduino
+ *  https://www.youtube.com/watch?v=8ua3U3xwqJA&t=4s
+project name fading led
+description. a demonsration using arduino inpu ( external pushbtton
+on pin 2)
+to control an output ( led on pin 9)
+
+*/
+
+
+#include <CapacitiveSensor.h>
+
+
+
+ const int led1 = 3; // red led  indicator
+ const int led2 = 11; // green
+ const int led3 = 9; // blue
+
+
+
+const int cap1 = 12;
+const int cap2 = 2; // cap sensor input pin  
+
+const int cap3 = 5; //red white sensor wires
+const int cap4 = 6;
+
+const int cap5 = 7; //green white sensor wires
+const int cap6 = 8;
+
+const int sensibil1 = 600;  // varies depending on the resistor and size of the capacitive surface, change to test. 
+const int sensibil2 = 600;  // varies depending on the resistor and size of the capacitive surface, change to test.
+const int sensibil3 = 600;  // varies depending on the resistor and size of the capacitive surface, change to test.
+
+
+
+CapacitiveSensor cs1 = CapacitiveSensor(cap1,cap2);     
+CapacitiveSensor cs2 = CapacitiveSensor(cap3,cap4);
+CapacitiveSensor cs3 = CapacitiveSensor(cap5,cap6);
+
+
+bool on1 = 0;
+bool on2 = 0;
+bool on3 = 0;
+
+
+
+void setup()                    
+{
+  cs1.set_CS_AutocaL_Millis( 0xFFFFFFFF);   
+  cs2.set_CS_AutocaL_Millis( 0xFFFFFFFF); 
+  cs3.set_CS_AutocaL_Millis( 0xFFFFFFFF); 
+ 
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+
+
+}
+
+
+void loop()  
+  
+{
+  long cap10 =  cs1.capacitiveSensor(30);  // call function that gives you capacitance ... 
+  if (cap10 > sensibil1 ) //... if it is greater than stipulated, turns on or off the light,
+  {if (on1)
+    {on1 = !on1;
+      
+     for (int i = 255; i >=0; i -= 5){
+     // loop is to gradually dim the led. During this period (2sec) the caption is not called, 
+      // which ensures that the lamp will not blink and erase again. The same goes for the other for. 
+      {analogWrite(led1, i);
+        delay(5); // delay speed increment higher number for slower 
+      }
+    } 
+    }  
+    else
+     {on1 = !on1;
+      for (int i = 0; i <= 255; i += 5){
+   
+      {
+        analogWrite(led1, i);
+        delay(5);
+      }
+    }
+   }
+  }  
+  
+   long cap20 =  cs2.capacitiveSensor(30);  // call function that gives you capacitance ... 
+ 
+
+  if (cap20 > sensibil2 ) //... if it is greater than stipulated, turns on or off the light,
+  
+    if (on2)
+    {
+      on2 = !on2;
+    
+    
+     for (int i2 = 255; i2 >=0; i2 -= 5){
+     // loop is to gradually dim the led. During this period (2sec) the caption is not called, 
+      // which ensures that the lamp will not blink and erase again. The same goes for the other for. 
+      {
+        analogWrite(led2, i2);
+        delay(5); // delay speed increment higher number for slower 
+      }
+    } 
+    }  
+    else
+    
+     {
+    
+      on2 = !on2;
+    
+    
+      for (int i2 = 0; i2 <= 255; i2 += 5){
+   
+      {
+        analogWrite(led2, i2);
+        delay(5);
+      }
+    }
+   }  
+
+
+          long cap30 =  cs3.capacitiveSensor(30);  // call function that gives you capacitance ... 
+ 
+
+  if (cap30 > sensibil3 ) //... if it is greater than stipulated, turns on or off the light,
+  {
+    if (on3)
+    {
+      on3 = !on3;
+    
+    
+     for (int i3 = 255; i3 >=0; i3 -= 5){
+     // loop is to gradually dim the led. During this period (2sec) the caption is not called, 
+      // which ensures that the lamp will not blink and erase again. The same goes for the other for. 
+      {
+        analogWrite(led3, i3);
+        delay(5); // delay speed increment higher number for slower 
+      }
+    } 
+    }  
+    else
+    
+     {
+    
+      on3 = !on3;
+    
+    
+      for (int i3 = 0; i3 <= 255; i3 += 5){
+   
+      {
+        analogWrite(led3, i3);
+        delay(5);
+      }
+  } 
+  }
+  }
+}
+
+
+  
+
+
+
+ 
+      
+ 
+
+
